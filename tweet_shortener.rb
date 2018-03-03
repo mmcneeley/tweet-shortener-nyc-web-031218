70123940543820f3ad52475cf8e0a04523341bb7
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:39:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 
 def word_substituter(tweet)
   puts tweet
-  tweet_array = tweet.split
+  tweet_array = tweet.to_s.split
   puts tweet_array.inspect
   tweet_array.map do |word|
      if dictionary.keys.to_s.include?(word)
