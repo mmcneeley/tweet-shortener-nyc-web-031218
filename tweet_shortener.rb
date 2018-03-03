6e0b999a9198e23b5fe1893da00c782612f97722
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 16:16:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,9 +40,17 @@ def selective_tweet_shortener(tweet)
     end
 end
 
+#def shortened_tweet_truncator(tweet)
+#  if word_substituter(tweet).length > 140
+#    word_substituter(tweet)[0...136] + "..."
+#  else
+#    tweet
+#  end
+#end
+
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0...136] + "..."
+    word_substituter(tweet)[0..136] + '...'
   else
     tweet
   end
