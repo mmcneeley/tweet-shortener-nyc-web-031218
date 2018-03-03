3f32e314b60803b8a5570a1a083c54216aeb63a4
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:28:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ end
 def word_substituter(tweet)
   tweet_array = tweet.split
   puts tweet_array.inspect
-  tweet_array.map do |word, x|
+  tweet_array.map do |word|
      if dictionary.keys.to_s.include?(word)
       #puts "woopee"
       dictionary[:word]
