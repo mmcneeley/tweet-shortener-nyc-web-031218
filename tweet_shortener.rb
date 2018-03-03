b315cfac4e31da6b39c86b50cc185279976e03c2
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:43:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,14 +29,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet)
-  puts "hey, this is a test"
-#  puts tweet
-#  #puts tweet.inspect
-#  if tweet.length > 140
-#    #puts "less than 140"
-#    tweet
-#  else
-    #puts "oh shit, bigger than 140"
-#    word_substituter(tweet)
-#  end
+  if tweet.length > 140
+    word_substituter(tweet)
+  end
 end
