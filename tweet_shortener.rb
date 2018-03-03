c0bafcc36b6fbf8fff69499465bf2b126cb3f85c
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:49:06 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,9 +16,10 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  puts tweet.class
+  puts "#{tweet.class} is the class"
+  puts "#{tweet.length} is the length"
   tweet_array = tweet.to_s.split
-  puts tweet_array.inspect
+  puts "#{tweet_array.inspect} is the new array
   tweet_array.map do |word|
      if dictionary.keys.to_s.include?(word)
       dictionary[word.to_sym]
