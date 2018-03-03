aa5d94e0939ab424acc03a55eb669a8b7c040e58
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:28:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def dictionary
+  {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  puts tweet_array.inspect
+  tweet_array.map.with_index do |word, x|
+     if dictionary.keys.to_s.include?(word)
+      #puts "woopee"
+      dictionary[:word]
+     else
+       #puts "weee"
+      word
+     end
+   end.join(" ")
+end
