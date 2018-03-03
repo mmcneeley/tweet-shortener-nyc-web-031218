02f3d56632d19c479a3fc33138152cc81d08732d
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 15:59:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,8 +30,8 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet)
-  tweet.each do |tweet|
-    puts word_substituter(tweet)
+  tweet.each do |t|
+    puts word_substituter(t)
   end
 end
 
